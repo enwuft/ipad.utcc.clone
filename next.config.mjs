2f4bd20478b792e70be6a00ea55825f524fb7d0e
ENWUFT <enwuft@gmail.com>
2024-03-15 23:22:35 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "admissions.utcc.ac.th",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
